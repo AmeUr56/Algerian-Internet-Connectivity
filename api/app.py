53bb28db8a2aed99d77ccb70f411874d77f37d17
Ameur56 <boughershaouari@gmail.com>
2024-12-30 11:45:44 +0100

    Modified API

--- api/app.py
+++ api/app.py
@@ -1,27 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
  
-from typing import List
-
-# Define a model for the individual record items
-class Record(BaseModel):
-    Year: str
-    Quarter: str
-    type: str
-    NumberRecords:str
-    Devices: str
-    Tests: str
-    Avg_Avg_U_Kbps: str
-    Avg_Avg_D_Kbps: str
-    Avg_Lat_Ms: int
-    Avg_Pop2005: str
-    Rank_Upload: int
-    Rank_Download: int
-    Rank_Latency: int
-
-# Define the Data model for the request body
-class Data(BaseModel):
-    content: List[Record]  # Expecting a list of records
+from typing import List,Dict,Any
     
 # Create a FastAPI instance
 app = FastAPI()
@@ -34,15 +14,15 @@ processed_data = None
 @app.post("/upload/{type}",
           summary="Upload Data",
           description="Upload Raw or Processed Data to the API")
-async def upload(type:str,data:Data):
+async def upload(type:str,data:Dict[str,Any]):
     if type not in ["raw","processed"]:
         raise HTTPException(status_code=404,detail="Invalid Type")
     if type == "raw":
         global raw_data
-        raw_data = data.content
+        raw_data = data["content"]
     else:
         global processed_data
-        processed_data = data.content
+        processed_data = data["content"]
         
     raise HTTPException(status_code=200,detail="Upload Data Successfully")
 
@@ -57,8 +37,8 @@ async def download(type:str):
     if type == "raw":
         if raw_data is None:
             raise HTTPException(status_code=404,detail="No Raw Data Uploaded")   
-        return Data(content=raw_data)
+        return raw_data
     else:
         if processed_data is None:
             raise HTTPException(status_code=404,detail="No Processed Data Uploaded")   
-        return Data(content=processed_data)
+        return processed_data
